c02430a824efadaceb221412d8d94bf59f7f225b
2pizzzza <amit.kurbanov.123.com@gmail.com>
2024-04-26 11:02:04 +0600

    change tab layout

--- app/src/main/java/com/example/wearherreport/screens/MainCard.kt
+++ app/src/main/java/com/example/wearherreport/screens/MainCard.kt
@@ -1,6 +1,5 @@
 package com.example.wearherreport.screens
 
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,7 +7,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
