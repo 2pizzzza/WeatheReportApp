afc2a3f2d50c377ea224424b591520c091426943
2pizzzza <amit.kurbanov.123.com@gmail.com>
2024-04-28 23:09:39 +0600

    fun:getData

--- app/src/main/java/com/example/wearherreport/MainActivity.kt
+++ app/src/main/java/com/example/wearherreport/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.wearherreport
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -11,14 +12,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.wearherreport.screens.MainCard
 import com.example.wearherreport.screens.TabLayout
 
+const val API_KEY = "d057081383a04a0eb0c133440242804"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            getData("Karakol", this)
             Image(
                 modifier = Modifier
                     .fillMaxSize()
@@ -36,8 +42,30 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-private fun getData(city: String, context:Context){
+private fun getData(city: String, context: Context) {
+    val url = "https://api.weatherapi.com/v1/forecast.json?" +
+            "key=$API_KEY&" +
+            "q=$city&" +
+            "days=3&" +
+            "aqi=no&" +
+            "alerts=no"
 
+    val queue = Volley.newRequestQueue(context)
+
+    val sRequest = StringRequest(
+        Request.Method.GET,
+        url,
+        {
+            response ->
+            Log.d("errorVolley", response)
+
+        },
+        {
+            Log.d("errorVolley", "$it")
+        }
+    )
+
+    queue.add(sRequest)
 }
 
 
